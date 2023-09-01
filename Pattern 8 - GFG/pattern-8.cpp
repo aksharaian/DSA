d110b47c8558442d9952e95996c7406cb2c324b0
Akshara Sharma <akshara582582@gmail.com>
2023-09-01 15:55:23 +0530

    Added solution - LeetHub

--- Pattern 8 - GFG/pattern-8.cpp
+++ Pattern 8 - GFG/pattern-8.cpp
@@ -16,7 +16,7 @@ public:
 	         for(int j=0; j< 2*n-(2*i +1); j++){
 	             cout<<"*";
 	         }
-	         for(int j=0; j<n; j++){
+	         for(int j=0; j<i; j++){
 	             cout<< " ";
 	         }
 	         cout<<endl;
